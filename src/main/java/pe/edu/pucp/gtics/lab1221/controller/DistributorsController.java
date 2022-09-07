a8e13448d1b9a985d73f4ee5030ddb50af13cff2
MarceloJon <mrojasl@pucp.edu.pe>
2022-09-06 20:39:55 -0500

    distributorscontroller & repository

--- src/main/java/pe/edu/pucp/gtics/lab1221/controller/DistributorsController.java
+++ src/main/java/pe/edu/pucp/gtics/lab1221/controller/DistributorsController.java
@@ -2,9 +2,16 @@ package pe.edu.pucp.gtics.lab1221.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pe.edu.pucp.gtics.lab1221.entity.Distributors;
 import pe.edu.pucp.gtics.lab1221.repository.DistributorsRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/distribuidoras")
@@ -15,24 +22,37 @@ public class DistributorsController {
 
 
     @GetMapping(value = {"/lista"})
-    public String listaDistribuidoras (){
-        return "";
-    };
+    public String listaDistribuidoras (Model model){
+        List<Distributors> listaDistributors = distributorsRepository.listarDistributorsPorNombre();
+        model.addAttribute("listaDistributors",listaDistributors);
+        return "distribuidoras/lista";
+    }
 
-    public String editarDistribuidoras(){
-        return "";
-    };
+    @GetMapping("/editar")
+    public String editarDistribuidoras(Model model, @RequestParam("id") Integer id){
+        Optional<Distributors> optionalDistributors = distributorsRepository.findById(id);
+        if (optionalDistributors.isPresent()){
+            Distributors distributors = optionalDistributors.get();
+            model.addAttribute("distribuidora",distributors);
+        }
+        return "distribuidoras/editar";
+    }
 
+    @GetMapping("/nuevo")
     public String nuevaDistribuidora(){
-        return "";
-    };
+        return "distribuidoras/nuevo";
+    }
 
-    public String guardarDistribuidora(){
-        return "";
-    };
+    @PostMapping("/guardar")
+    public String guardarDistribuidora(Distributors distributors){
+        distributorsRepository.save(distributors);
+        return "redirect:/distribuidoras/lista";
+    }
 
-    public String borrarDistribuidora(){
-        return "";
-    };
+    @GetMapping("/borrar")
+    public String borrarDistribuidora(@RequestParam("id") Integer id){
+        distributorsRepository.deleteById(id);
+        return "redirect:/distribuidoras/lista";
+    }
 
 }
--- src/main/java/pe/edu/pucp/gtics/lab1221/repository/DistributorsRepository.java
+++ src/main/java/pe/edu/pucp/gtics/lab1221/repository/DistributorsRepository.java
@@ -2,11 +2,15 @@ package pe.edu.pucp.gtics.lab1221.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pe.edu.pucp.gtics.lab1221.entity.Distributors;
 
+import java.util.List;
+
 @Repository
 public interface DistributorsRepository extends JpaRepository<Distributors, Integer> {
-
+    @Query(nativeQuery = true,value = "select * from distribuidoras order by nombre;")
+    List<Distributors> listarDistributorsPorNombre();
 
 }
