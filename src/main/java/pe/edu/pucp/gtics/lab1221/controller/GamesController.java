2fcf68d5e2ad5dda4d55384e49d36ba0f1f3ee3c
Miguel <a20190050@pucp.edu.pe>
2022-09-06 20:43:16 -0500

    raaa

--- src/main/java/pe/edu/pucp/gtics/lab1221/controller/GamesController.java
+++ src/main/java/pe/edu/pucp/gtics/lab1221/controller/GamesController.java
@@ -1,5 +1,10 @@
 package pe.edu.pucp.gtics.lab1221.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
 public class GamesController {
 
     public String listaJuegos (){
@@ -10,8 +15,16 @@ public class GamesController {
         return "";
     };
 
-    public String guardarJuegos(){
-        return "";
-    };
+    GetMapping("/editar")
+    public String guardarJuegos(Model model, @RequestParam("id") int id){
+        Optional<Games> optGames = gamesRepository.findById(id);
+        if (optShipper.isPresent()){
+            Games games = optGames.get();
+            model.addAttribute("games", games);
+            return "juegos/editar";
+        }else{
+            return "redirect:/juegos/lista";
+        }
+    }
 
 }
