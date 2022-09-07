aef91c699a805e284098d6c2cc212571d21434bc
Stefhanie Jaramillo <a20190674@pucp.edu.pe>
2022-09-06 20:47:29 -0500

    mapeo

--- src/main/java/pe/edu/pucp/gtics/lab1221/controller/GamesController.java
+++ src/main/java/pe/edu/pucp/gtics/lab1221/controller/GamesController.java
@@ -1,6 +1,16 @@
 package pe.edu.pucp.gtics.lab1221.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import pe.edu.pucp.gtics.lab1221.entity.Games;
+import pe.edu.pucp.gtics.lab1221.repository.GamesRepository;
+
 public class GamesController {
+
+    @Autowired
+    GamesRepository gamesRepository;
+
+
 
     public String listaJuegos (){
         return "";
@@ -9,9 +19,10 @@ public class GamesController {
     public String editarJuegos(){
         return "";
     };
-
-    public String guardarJuegos(){
-        return "";
-    };
+    @PostMapping("juegos/guardar")
+    public String guardarJuegos(Games games){
+            gamesRepository.save(games);
+            return "redirect:/shipper/list";
+    }
 
 }
