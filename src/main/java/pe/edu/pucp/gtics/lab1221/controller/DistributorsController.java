102d51c7f3619d1b5e64de39725ddf9b1ca83c66
Victor Calderon <victor.calderon@pcup.edu.pe>
2022-09-06 20:13:36 -0500

    Ejer1

--- src/main/java/pe/edu/pucp/gtics/lab1221/controller/DistributorsController.java
+++ src/main/java/pe/edu/pucp/gtics/lab1221/controller/DistributorsController.java
@@ -1,7 +1,20 @@
 package pe.edu.pucp.gtics.lab1221.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pe.edu.pucp.gtics.lab1221.repository.DistributorsRepository;
+
+@Controller
+@RequestMapping("/distribuidoras")
 public class DistributorsController {
 
+    @Autowired
+    DistributorsRepository distributorsRepository;
+
+
+    @GetMapping(value = {"/lista"})
     public String listaDistribuidoras (){
         return "";
     };
